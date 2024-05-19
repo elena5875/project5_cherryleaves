13f2daeeaec55930cf683d8d81dc8f92cc2b81e0
Elena freire <elenafreire75@gmail.com>
2024-05-19 20:37:18 +0000

    change requirements to fit heroku deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: sh setup.sh && streamlit run app.py
+web: streamlit run app_pages/multi_page.py
+
--- app_pages/Data_visualization.py
+++ app_pages/Data_visualization.py
@@ -58,25 +58,27 @@ def display_images(directory, image_files, label):
 
     # Display selected images with adjustable width
     selected_image_index = st.session_state[f"index_{label.lower().replace(' ', '_')}"]
-    col1, col2 = st.beta_columns(2)
+    cols = st.columns(2)
 
     # Show first image
     selected_image_path1 = os.path.join(directory, image_files[selected_image_index])
     selected_image1 = Image.open(selected_image_path1)
     image_width = st.selectbox(f"Choose Image Width for {label} (pixels):", options=[100, 200, 300], index=0, key=f"selectbox_{label.lower().replace(' ', '_')}")
-    col1.image(selected_image1, caption=f"{label} Image {selected_image_index+1}", width=image_width, use_column_width=True)
+    cols[0].image(selected_image1, caption=f"{label} Image {selected_image_index+1}", width=image_width, use_column_width=True)
 
     # Show second image
     selected_image_index2 = selected_image_index + 1 if selected_image_index + 1 < len(image_files) else 0
     selected_image_path2 = os.path.join(directory, image_files[selected_image_index2])
     selected_image2 = Image.open(selected_image_path2)
-    col2.image(selected_image2, caption=f"{label} Image {selected_image_index2+1}", width=image_width, use_column_width=True)
+    cols[1].image(selected_image2, caption=f"{label} Image {selected_image_index2+1}", width=image_width, use_column_width=True)
 
     # Show Next and Show Previous buttons
-    col1, col2, col3 = st.beta_columns([1, 2, 1])
-    if col2.button("Show Previous", key=f"prev_button_{label.lower().replace(' ', '_')}"):
+    prev_button = cols[0].button("Show Previous", key=f"prev_button_{label.lower().replace(' ', '_')}")
+    next_button = cols[1].button("Show Next", key=f"next_button_{label.lower().replace(' ', '_')}")
+
+    if prev_button:
         st.session_state[f"index_{label.lower().replace(' ', '_')}"] = max(0, selected_image_index - 1)
-    if col2.button("Show Next", key=f"next_button_{label.lower().replace(' ', '_')}"):
+    elif next_button:
         st.session_state[f"index_{label.lower().replace(' ', '_')}"] = (selected_image_index + 1) % len(image_files)
 
 # Function to create image montage
